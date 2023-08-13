7db76bab96be148110b8edb3f618514f2d6f2962
Soyeon Kim <kksoyeon61@gmail.com>
2023-08-13 21:32:31 +0200

    added questions

--- Coding test Day1/index.js
+++ Coding test Day1/index.js
@@ -1,5 +1,5 @@
 // 문제설명
-//정수 num1 과 num2가 주어질때, num1 과 num2의 합을 return 하도록 solution 함수를 완성해주세요.
+// 1.정수 num1 과 num2가 주어질때, num1 과 num2의 합을 return 하도록 solution 함수를 완성해주세요.
 
 //제한사항
 //- 50,000 < num1 < 50,000
@@ -7,6 +7,21 @@
 
 function solution(num1,num2){ // solution 함수는 여러번 동작한다
     // num1 => 어떤 값으로 대체가 가능
-    var answer = -1;
+    return num1 +  num2;
+}
+
+//2. 두 수의 차 
+// 문제설명 
+// 정수 num1 과 num2 가 주어질때, num1에서 num2를 뺀 값을 return 하도록 solution 함수를 완성해주세요.
+
+//제한사항
+// -5000 < num1 < 50000
+// -5000 < num2 < 50000
+
+function solution(num1 , num2) {
+    return num1 - num2;
+}
+function solution(num1, num2) {
+    var answer = num1 - num2;
     return answer;
 }
