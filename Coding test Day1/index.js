f4f7a9084798b77a67467869fc5320b055549e05
Soyeon Kim <kksoyeon61@gmail.com>
2023-08-13 21:37:15 +0200

    4 questions and answer updated

--- Coding test Day1/index.js
+++ Coding test Day1/index.js
@@ -25,3 +25,23 @@ function solution(num1, num2) {
     var answer = num1 - num2;
     return answer;
 }
+
+//3. 두 수의 곱
+// 정수num1, num2가 매개변수 주어집니다. num1 과 num2 를 곱한 값을 return 하도록 solution 함수를 완성해주세요
+
+function solution (num1, num2) {
+    return num1*num2;
+}
+function solution(num1, num2){
+    var answer = num1 * num2;
+    return answer;
+}
+
+// 4.몫 구하기
+// 정수 num1, num2가 매개변수로 주어질때 num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+function solution(num1, num2) {
+    return Math.floor(num1/ num2) // 결과가 딱 나눠 떨어지지 않으면 틀리다고 뜸
+
+    // Math.floor(19/7) 소숫점 뒷자리 버림 함수
+}
