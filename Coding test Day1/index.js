08d6b455c85f26552b2a52fab5fe1ed1ee944b61
Soyeon Kim <kksoyeon61@gmail.com>
2023-08-13 20:31:59 +0200

    solution function practice

---
+++ Coding test Day1/index.js
@@ -0,0 +1,12 @@
+// 문제설명
+//정수 num1 과 num2가 주어질때, num1 과 num2의 합을 return 하도록 solution 함수를 완성해주세요.
+
+//제한사항
+//- 50,000 < num1 < 50,000
+//- 50,000 < num2 < 50,000
+
+function solution(num1,num2){ // solution 함수는 여러번 동작한다
+    // num1 => 어떤 값으로 대체가 가능
+    var answer = -1;
+    return answer;
+}
