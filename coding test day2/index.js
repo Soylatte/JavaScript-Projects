6e3236087efbc58b9857c3b3382a0b4550161208
Soyeon Kim <kksoyeon61@gmail.com>
2023-08-20 11:47:16 +0200

    second question

--- coding test day2/index.js
+++ coding test day2/index.js
@@ -6,6 +6,10 @@
 
 function solution (num1, num2){
     var answer = Math.floor(num1 / num2 * 1000);
+    console.log("hello");
+
+    // 1. number -> string
+    // 2. 10 진수 추론을 위해 string 앞 2글자나 0x나 0x인지 비교 
     return answer; 
 }
 
@@ -13,3 +17,16 @@ function solution (num1, num2){
 // 나눈 값에 1000을 곱한다
 // 그 값에 정수부분을 구한다.
 // parseInt 도 사용가능 Math.floor 같음 정수부분 찾을때 사용
+
+//2. 숫자비교하기
+
+function solution (num1, num2) {
+   if (true){
+    return 1; // true 면 여기서 코드가 끝남
+   } else {
+    return -1;   // false 면 여기서 끝남
+}
+
+//1.두 수가 같은지 비교
+//2. 비교 결과에 따라 1, -1
+
